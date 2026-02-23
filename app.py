700f01ed329238dfdcc4f3e2da373324aa32bf72
kmp71793-hub <kmp71793@gmail.com>
2026-02-23 23:07:52 +0530

    final

--- app.py
+++ app.py
@@ -249,6 +249,7 @@ def predict():
 # ---------------- MAIN ----------------
 
 if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
     with app.app_context():
         db.create_all()
 
