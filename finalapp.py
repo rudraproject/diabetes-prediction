e558854cf9602ef14290cce8c23311f6087f61d8
kmp71793-hub <kmp71793@gmail.com>
2026-02-26 21:00:31 +0530

    dead

--- finalapp.py
+++ finalapp.py
@@ -8,7 +8,7 @@ import os
 app = Flask(__name__)
 
 # ---------------- SECRET KEY ----------------
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY","devkey123")
 
 # ---------------- DATABASE CONFIG ----------------
 database_url = os.environ.get("DATABASE_URL")
